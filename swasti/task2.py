1751ee05ceb9506280201a8feb0a09291a5d3010
swastis05 <21singhalsw@hazelwick.org>
2026-09-18 12:18:47 +0000

    18/9/26

--- swasti/task2.py
+++ swasti/task2.py
@@ -2,10 +2,15 @@ digit = int(input("Enter a 3 digit number: "))
 hundreds = digit // 100 
 tens1 = digit % 100
 tens2 = tens1 // 10
-units = tens2 % 10
+units = tens1 % 10
 sum = hundreds + tens2 + units 
 reversed = str(units) + str(tens2) + str(hundreds)
+print("Hundreds: " , hundreds)
+print("Tens: ", tens2)
+print("Units: ", units)
+print("Sum of digits: ", sum)
+print("Reversed: " + reversed)
 if digit % 2 == 0: 
     print("Even number: True")
 else: 
-    print("Even number")
+    print("Even number: False")
